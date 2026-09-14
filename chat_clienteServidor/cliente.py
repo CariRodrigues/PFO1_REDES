abb52e8b87cc878399cc87a5d517d98519833cac
Carina Rodrigues <crodrigues.prog@gmail.com>
2026-09-14 16:57:40 -0300

    Carga de archivos

---
+++ chat_clienteServidor/cliente.py
@@ -0,0 +1,21 @@
+import socket
+
+cliente = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+cliente.connect(('127.0.0.1', 5000))
+
+mensaje = ""
+while mensaje != "éxito":
+    mensaje = input("Ingrese un mensaje: ")
+
+    if mensaje == "éxito":
+        break
+
+
+    mensajebytes = mensaje.encode('utf-8')
+    cliente.send(mensajebytes)
+
+    respuesta = cliente.recv(1024).decode("utf-8")
+    print(respuesta)
+
+    
---
+++ chat_clienteServidor/servidor.py
@@ -0,0 +1,100 @@
+import socket
+import sqlite3
+from datetime import datetime
+
+#Función que prepara la base de datos
+def inicializar_db():
+    conexion_db = None
+    try:
+        conexion_db = sqlite3.connect('chat.db')
+        cursor = conexion_db.cursor()
+        #tabla p/ guardar los msj
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS mensajes (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            contenido TEXT,
+            fecha_envio TEXT,
+            ip_cliente TEXT)
+        ''')
+        conexion_db.commit()
+        return True
+    except sqlite3.Error:
+        print("Ocurrió un error al inicializar la DB")
+        return False
+    finally:
+        if conexion_db is not None:
+            conexion_db.close()
+
+#función que almacena los mensajes en la base (en la tabla creada)
+def guardar_mensaje(contenido, fecha_envio, ip_cliente):
+        conexion_db = None
+        try:
+            conexion_db = sqlite3.connect('chat.db')
+            cursor = conexion_db.cursor()
+
+            cursor.execute('''
+                INSERT INTO mensajes (contenido, fecha_envio, ip_cliente)
+                VALUES (?, ?, ?) 
+            ''', (contenido, fecha_envio, ip_cliente))
+
+            conexion_db.commit()
+            
+        except sqlite3.Error:
+            print("Ocurrió un error al guardar el mensaje en la base de datos")
+        finally:
+            if conexion_db is not None:
+                conexion_db.close()
+
+
+def inicializar_servidor():
+#configuración del socket (TCP/IP)
+    servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        servidor.bind(('127.0.0.1' , 5000))
+    except OSError:
+        print("No se pudo iniciar el servidor porque el puerto está ocupado")
+        return None
+
+    servidor.listen()
+    print("servidor escuchando en localhost 5000")
+    return servidor
+
+#acepta la conexión con el cliente, recibe mensajes y responde
+def aceptar_conexion(servidor):
+    conexion, direccion = servidor.accept()
+    print("cliente conectado", direccion)
+
+    while True: 
+            datos = conexion.recv(1024)
+            if datos == b'':
+                break
+    
+            datos = datos.decode("utf-8")
+            fecha_hora = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+            guardar_mensaje(datos, fecha_hora, direccion[0])
+    
+            print(f"Mensaje recibido:{datos}, {fecha_hora}")
+    
+            respuesta = f"Mensaje recibido: {fecha_hora}"    
+            conexion.sendall(respuesta.encode('utf-8'))
+
+    conexion.close()
+
+
+def main():
+
+    db_ok = inicializar_db()
+
+    if not db_ok:
+        print("Error al inicializar la base de datos")
+        return
+        
+    servidor = inicializar_servidor()
+    if servidor is None:
+        return
+
+    aceptar_conexion(servidor)
+    
+if __name__ == "__main__":
+    main()
+
